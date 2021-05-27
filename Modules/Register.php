6ea62ac6089531583637acf7ca7002e491024819
Paweł Sroka <P.sroka@onet.eu>
2021-05-27 14:00:47 +0200

    +

--- Modules/Register.php
+++ Modules/Register.php
@@ -14,6 +14,16 @@ if(isset($_POST['phone'])) {
 if(isset($_POST['passw'])) {
     $passw= $_POST['passw'];
 }
+require('db.php');
+
+$conn=new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error) {
+    die('Oops! Something went wrong: ' . $conn->connect_error);
+}
+
+$sql= "INSERT INTO clients (name, surname, Email, phone_number, password) VALUES ('$name', '$surname', '$email', '$phone', '$passw')";
+
+$conn->close();
 
 header('Location: ./Dashboard.html');
 ?>
