c8771848d52b34d01b6fc5211248bb65a1ea76c5
Paweł Sroka <P.sroka@onet.eu>
2021-06-02 19:34:06 +0200

    member update

--- Members.php
+++ Members.php
@@ -19,15 +19,15 @@ require ('Components/Navbar.php');
 
 <!-- Main Panel -->
 <div class="container-fluid">
-    <table class="table">
+    <table class="table" id="myTable">
         <thead class="table-primary">
         <tr>
-            <th>Nr.</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Position</th>
-            <th>Team</th>
-            <th>Location</th>     
+            <th onclick="sortTable(0)">Nr.</th>
+            <th onclick="sortTable(1)">Name</th>
+            <th onclick="sortTable(2)">Email</th>
+            <th onclick="sortTable(3)">Position</th>
+            <th onclick="sortTable(4)">Team</th>
+            <th onclick="sortTable(5)">Location</th>     
             <th> <input class="form-control" type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."></th>       
         </tr>  
         </thead>
@@ -57,7 +57,7 @@ require ('Components/Navbar.php');
                       data-mdb-toggle="dropdown"
                       aria-expanded="false"
                     >
-                      Dropdown button
+                      Manage
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                       <li><a class="dropdown-item" href="#">Action</a></li>
@@ -71,6 +71,94 @@ require ('Components/Navbar.php');
         </tbody>
     </table>
 </div>
+
+<script>
+
+// Search Bar
+
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1,2,3,4,5];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+// Search Bar
+
+// Sort by
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
+//Sort by
+</script>
 <!-- Main Panel -->
 <!-- <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> -->
 </body>
