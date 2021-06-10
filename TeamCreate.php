48f9e1000a56975c499f8fe71f81c8ef9f2b6dd8
Paweł Sroka <P.sroka@onet.eu>
2021-06-10 22:32:15 +0200

    Update TeamCreate.php

--- TeamCreate.php
+++ TeamCreate.php
@@ -10,6 +10,8 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/mdb.min.css" type="text/css">
     <script src="js/mdb.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="stylesheet" href="css/index.css" type="text/css">
 </head>
 <body>
     
