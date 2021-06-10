02b36596d6520bf81470c6f4af57ed0f6333a703
Paweł Sroka <P.sroka@onet.eu>
2021-06-10 22:45:20 +0200

    Update Login.php

--- Modules/Login.php
+++ Modules/Login.php
@@ -7,19 +7,17 @@ require('db.php');
 $email = $_POST['Email'];
 $passw = $_POST['passw'];
 
-$sql="SELECT ID FROM clients WHERE Email = '$email' AND password = '$passw'";
-
-$result = $conn->query($sql);
-
-while ($row = $result->fetch_assoc()) {
-    $ID = $row['ID'];
-    if(!empty($ID)) {
-        $_SESSION['ID'] = $row['ID'];
-        header('Location:../Dashboard.php');
-    }else{
-        header('Location:../Login.html');
-    }
-    
+$stmt=$conn->prepare("SELECT ID FROM clients WHERE Email =? AND password = ?");
+$stmt->bind_param('ss', $email, $passw);
+$stmt->execute();
+$stmt->bind_result($id);
+$stmt->fetch();
+if(isset($id)) {
+    $_SESSION['ID'] = $row['ID'];
+    header('Location:../Dashboard.php');
+}else {
+    header('Location:../Login.html');
 }
+
 $conn->close();
 ?>
